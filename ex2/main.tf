35e53ceb31c856e9dde0d75e17ce8e8704567913
LucaRoveroni <lucaroveroni@protonmail.com>
2023-10-24 17:47:50 +0200

    Fixed sg and routes

--- ex2/main.tf
+++ ex2/main.tf
@@ -320,24 +320,20 @@ resource "aws_lb" "alb" {
 resource "aws_security_group" "alb-sec-group" {
   name   = "alb-sec-group"
   vpc_id = aws_vpc.vpc-2.id
-}
 
-resource "aws_security_group_rule" "ingress_alb_traffic" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  security_group_id = aws_security_group.alb-sec-group.id
-  cidr_blocks       = ["0.0.0.0/0"]
-}
+  ingress {
+    from_port         = 80
+    to_port           = 80
+    protocol          = "tcp"
+    cidr_blocks       = ["0.0.0.0/0"]   
+  }
 
-resource "aws_security_group_rule" "egress_alb_traffic" {
-  type              = "egress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  security_group_id = aws_security_group.alb-sec-group.id
-  cidr_blocks       = ["0.0.0.0/0"]
+  egress {
+    from_port         = 0
+    to_port           = 0
+    protocol          = "-1"
+    cidr_blocks       = ["0.0.0.0/0"]   
+  }
 }
 
 resource "aws_lb_listener" "http_listener" {
