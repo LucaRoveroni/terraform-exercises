33a876d98365f309b0f5b9d85e175c5c670a426d
LucaRoveroni <lucaroveroni@protonmail.com>
2023-10-25 10:14:23 +0200

    Fixed routes

--- ex2/main.tf
+++ ex2/main.tf
@@ -196,6 +196,13 @@ resource "aws_security_group" "webserver-sg-ec2" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 8081
     to_port     = 8081
