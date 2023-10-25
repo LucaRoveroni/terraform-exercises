1cb3df3782582a68ef21af51f736f9135e3fb5ca
LucaRoveroni <lucaroveroni@protonmail.com>
2023-10-25 11:44:57 +0200

    Added bastion and NAT

--- ex2/main.tf
+++ ex2/main.tf
@@ -229,10 +229,24 @@ resource "aws_security_group" "webserver-sg-ec2" {
   }
 
   egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["10.2.0.0/24"]
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [ "10.2.0.0/16" ]
   }
  
   tags = {
